043e6e328867fad874f8f33b5590d1b37662328d
xukuang <121598119@qq.com>
2022-08-10 13:54:51 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResponse('/index')
 
 #这里加了一行注释
+
+def login(request):
+    return redirect('/login')
+
+
 
 
